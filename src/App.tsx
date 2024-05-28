abd7bba171e6d47dadc418e278921f1fa49a1a22
Raikonif <raikonif@gmail.com>
2024-05-28 17:45:16 -0400

    useQuery class sec 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,43 @@
 import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 import { getData } from "./services/fetchData";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [number, setNumber] = useState<number | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | undefined>(undefined);
-  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
+  // const [number, setNumber] = useState<number | null>(null);
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
+  // const [error, setError] = useState<string | undefined>(undefined);
+  // const [key, forceRefetch] = useReducer((x) => x + 1, 0);
 
-  useEffect(() => {
-    setIsLoading(true);
-    getData()
-      .then((num) => {
-        setNumber(num);
-      })
-      .catch((err) => {
-        setError(err.message);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [key]);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   getData()
+  //     .then((num) => {
+  //       setNumber(num);
+  //     })
+  //     .catch((err) => {
+  //       setError(err.message);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, [key]);
+
+  const query = useQuery({ queryKey: ["randomNumber"], queryFn: getData });
 
   return (
     <div>
-      {isLoading ? (
+      {query.isLoading ? (
         <h1>Loading...</h1>
       ) : (
-        !error && (
-          <div>
-            <h4>Random Number: {number}</h4>
-          </div>
-        )
+        <h2>Random Number: {query.data} </h2>
       )}
-      {!isLoading && error && <h1>{error}</h1>}
-      <button onClick={forceRefetch} disabled={isLoading}>
-        {isLoading ? "..." : "Generate new number"}
+      {!query.isLoading && query.isError && (
+        <h3>Random Number: {`${query.error}`}</h3>
+      )}
+
+      <button onClick={() => query.refetch()} disabled={query.isLoading}>
+        {query.isLoading ? "..." : "Generate new number"}
       </button>
     </div>
   );
